4b93bf54fcec61a886d6785a1bed8bbb0c196ed9
OHZEKI Naoki <0h23k1.n40k1@gmail.com>
2024-04-25 21:56:19 +0900

    Impl timer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [time, setTime] = useState(format(new Date()));
+
+  setInterval(() => {
+    const now = new Date();
+    setTime(format(now));
+  });
+
   return (
     <main>
-      <h1> 2024/08/10 11:45:14.1919 </h1>
+      <h1> {time} </h1>
     </main>
   );
 }
+
+const format = (date: Date): string => {
+  const yyyy = date.getFullYear();
+  const mm = date.getMonth();
+  const dd = date.getDate();
+  const HH = date.getHours();
+  const MM = date.getMinutes();
+  const ss = date.getSeconds();
+  const SSS = date.getMilliseconds();
+  return `${yyyy}/${mm}/${dd} ${HH} ${MM} ${ss}.${SSS}`;
+};

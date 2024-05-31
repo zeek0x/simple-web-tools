d5d2e7ce50313b9f49fdebe5171a2fceb7f69b75
OHZEKI Naoki <0h23k1.n40k1@gmail.com>
2024-06-01 00:15:22 +0900

    Add stop-watch

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-row">
       <a rel="noopener noreferrer" href="milli-clock" className="hidden"></a>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
@@ -20,6 +20,30 @@ export default function Home() {
           <a
             target="_blank"
             href="milli-clock"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-orange-500 dark:hover:bg-orange-600 dark:focus:ring-orange-700"
+          >
+            Open
+          </a>
+        </div>
+      </div>
+
+      <a rel="noopener noreferrer" href="stop-watch" className="hidden"></a>
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <iframe src={"stop-watch"} loading="lazy" className="h-48 w-full" />
+        </a>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Stop Watch
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            A simple stop watch.
+          </p>
+          <a
+            target="_blank"
+            href="stop-watch"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-orange-500 dark:hover:bg-orange-600 dark:focus:ring-orange-700"
           >
             Open
---
+++ app/stop-watch/page.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const date = new Date();
+  const [time, setTime] = useState({
+    startAt: date,
+    updatedAt: date,
+    offset: 0,
+  });
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      if (isRunning) {
+        setTime({ ...time, updatedAt: new Date() });
+      }
+    }, 10);
+
+    return () => clearInterval(timerId);
+  }, [isRunning, time]);
+
+  return (
+    <main className="h-screen w-screen flex flex-col justify-center items-center">
+      <h1 className="text-[6vw] font-bold tabular-nums">
+        {format(
+          time.offset + time.updatedAt.getTime() - time.startAt.getTime()
+        )}
+      </h1>
+      <div>
+        <div className="flex justify-center space-x-4">
+          {!isRunning
+            ? createStartButton(() => {
+                setIsRunning(true);
+                const date = new Date();
+                setTime({ ...time, startAt: date, updatedAt: date });
+              })
+            : createPauseButton(() => {
+                setIsRunning(false);
+                const date = new Date();
+                setTime({
+                  startAt: date,
+                  updatedAt: date,
+                  offset:
+                    time.offset +
+                    time.updatedAt.getTime() -
+                    time.startAt.getTime(),
+                });
+              })}
+          {createResetButton(() => {
+            setIsRunning(false);
+            const date = new Date();
+            setTime({ startAt: date, updatedAt: date, offset: 0 });
+          })}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+const format = (timeMS: number): string => {
+  const ms = timeMS % 1000;
+  timeMS -= ms;
+  const s = (timeMS / 1000) % 60;
+  timeMS -= s * 1000;
+  const m = (timeMS / 1000 / 60) % 60;
+  timeMS -= m * 1000 * 60;
+  const h = timeMS / 1000 / 60 / 60;
+  timeMS -= h * 1000 * 60 * 60;
+
+  const SS = ("00" + ms).slice(-3, -1);
+  const ss = ("0" + s).slice(-2);
+  const MM = ("0" + m).slice(-2);
+  const hh = ("0" + h).slice(-2);
+  return `${hh}:${MM}:${ss}.${SS}`;
+};
+
+const createStartButton = (onClick: () => void) => {
+  return (
+    <button
+      onClick={onClick}
+      className="p-4 bg-green-500 rounded-full hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+      aria-label="Start"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        className="w-6 h-6 text-white"
+      >
+        <path fill="none" d="M6 5l9 5-9 5V5z" />
+      </svg>
+    </button>
+  );
+};
+
+const createPauseButton = (onClick: () => void) => {
+  return (
+    <button
+      onClick={onClick}
+      className="p-4 bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+      aria-label="Pause"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        className="w-6 h-6 text-white"
+      >
+        <rect x="7" y="5" width="3" height="14" />
+        <rect x="14" y="5" width="3" height="14" />
+      </svg>
+    </button>
+  );
+};
+
+const createResetButton = (onClick: () => void) => {
+  return (
+    <button
+      onClick={onClick}
+      className="p-4 bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+      aria-label="Reset"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth="2"
+        className="w-6 h-6 text-white"
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h12v12H6z" />
+      </svg>
+    </button>
+  );
+};
